eea2e4daa34c68bc31cbc363863f4f6bcc2caaf7
lingueeni <76748976+lingueeni@users.noreply.github.com>
2023-10-30 17:17:30 +0200

    finale

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import WeatherApp from "./componenets/WeatherApp/WeatherApp";
+
+const App = () => {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+};
+
+export default App;
